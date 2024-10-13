74f56aebe275e4f5f2a7eb7f99978728345d2422
blaizot <guillaume.blaizot@epitech.eu>
2024-10-13 21:57:59 +0200

    feat(enemies): add create_shooting_enemy method and update create_classic_enemy; modify enemy creation logic

--- src/client/Create_ennemies.hpp
+++ src/client/Create_ennemies.hpp
@@ -35,7 +35,34 @@ namespace ecs {
              * @param state State of the ennemy
              * @return void
              */
-            void create_enemy(Registry& registry,
+            void create_classic_enemy(Registry& registry,
+            float x, float y,
+            float speed,
+            const std::string& texturePath,
+            float minX, float maxX,
+            float minY, float maxY,
+            float angle = 0.0f, float radius = 0.0f, float angular_speed = 0.0f,
+            State state = State::Patrolling);
+
+            /**
+             * @brief Create a shooting ennemy
+             * 
+             * @param registry contains all the logic of the components
+             * @param x Position on the x axis
+             * @param y Position on the y axis
+             * @param speed Speed of the ennemy
+             * @param texturePath Path to the texture of the ennemy
+             * @param minX Minimum position on the x axis
+             * @param maxX Maximum position on the x axis
+             * @param minY Minimum position on the y axis
+             * @param maxY Maximum position on the y axis
+             * @param angle Angle of the ennemy
+             * @param radius Radius of the ennemy
+             * @param angular_speed Angular speed of the ennemy
+             * @param state State of the ennemy
+             * @return void
+             */
+            void create_shooting_enemy(Registry& registry,
             float x, float y,
             float speed,
             const std::string& texturePath,
